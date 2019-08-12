789a0cb22bfc16e52a5d2e1ce1cb84e47f7fb0d4
0-jam <28038981+0-jam@users.noreply.github.com>
2019-08-12 12:19:29 +0900

    Changed list size value

--- bogo.py
+++ bogo.py
@@ -6,7 +6,7 @@ def main():
     rng = random.SystemRandom(time.time())
 
     num_range = 10
-    list_size = 50
+    list_size = 10
 
     orig_list = [rng.randrange(num_range) for _ in range(list_size)]
     sorted_list = sorted(orig_list)
@@ -19,12 +19,11 @@ def main():
         count += 1
         elapsed_time = time.time() - start_time
 
-        print("list: {}, count: {}, elapsed time: {}sec".format(shuffled_list, count, elapsed_time), end='\r', flush=True)
+        print("list: {}, count: {}, elapsed time: {:.3f}sec".format(shuffled_list, count, elapsed_time), end='\r', flush=True)
 
         if shuffled_list == sorted_list:
+            print('')
             break
-
-    print("list: {}, count: {}, elapsed time: {}sec".format(shuffled_list, count, elapsed_time))
 
 
 if __name__ == "__main__":
